15302a7f720ba4f895e324a7e62b85a228ef4ecd
Naveen Kumar <er.crackerjack@gmail.com>
2024-09-17 14:52:12 +1200

    make fixes in main activity file regarding imports files

--- app/src/main/java/er/crackerjack/counterviewmodel/MainActivity.kt
+++ app/src/main/java/er/crackerjack/counterviewmodel/MainActivity.kt
@@ -1,27 +1,40 @@
 package er.crackerjack.counterviewmodel
 
+import CounterViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import er.crackerjack.counterviewmodel.ui.theme.CounterViewModelTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel: CounterViewModel by viewModels()
             CounterViewModelTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    CounterApp(
+                        viewModel,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +44,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun CounterApp(viewModel: CounterViewModel, modifier: Modifier = Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CounterViewModelTheme {
-        Greeting("Android")
+    Column (
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+        Text(
+            text = "Count: ${viewModel.count.value}"
+        )
+        Row (
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.Center
+
+        ){
+            Button(
+                onClick = { viewModel.increment() },
+            ) {
+                Text("Increment")
+            }
+            Spacer(
+                modifier = Modifier.width(8.dp)
+            )
+            Button(
+                onClick = { viewModel.decrement() }
+            ) {
+                Text("Decrement")
+            }
+        }
     }
 }
